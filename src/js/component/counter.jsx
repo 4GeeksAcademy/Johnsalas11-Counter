e3a24e5f8fc4215229bb8e672a47671cb8358d9a
John <salasduranjohn96@gmail.com>
2024-11-19 17:35:04 +0000

    Last Commit

--- src/js/component/counter.jsx
+++ src/js/component/counter.jsx
@@ -2,40 +2,34 @@ import React from "react";
 
 
 export const Counter = () => {
-    return (
-      <div className="contenedor-principal">
-        <div className="counter-container" id='crono'>00:00:00</div>
-      </div>
-    );
-};
-
-export const counter = document.querySelectorAll('crono');
-
   let [horas, minutos, segundos] = [0, 0, 0];
 
-  let intervaloDeTiempo;
-  intervaloDeTiempo = window.setInterval(actualizarCronometro(), 1000);
 
   function actualizarCronometro() {
-      segundos++;
-  
-      if (segundos / 60 === 1) {
-          segundos = 0;
-          minutos++;
-          
-          if (minutos/ 60 === 1){
-              minutos = 0;
-              horas++;
-          }
+    segundos++;
+
+    if (segundos / 60 === 1) {
+      segundos = 0;
+      minutos++;
+
+      if (minutos / 60 === 1) {
+        minutos = 0;
+        horas++;
       }
-      const segundosUnaCifra = asignarCifra(segundos) ;
-      const minutosUnaCifra = asignarCifra(minutos) ;
-      const horasUnaCifra = asignarCifra(horas) ;
-      counter.innerText = `${horasUnaCifra}:${minutosUnaCifra}:${segundosUnaCifra}`;
+    }
+    const segundosUnaCifra = asignarCifra(segundos);
+    const minutosUnaCifra = asignarCifra(minutos);
+    const horasUnaCifra = asignarCifra(horas);
+    document.getElementById("crono").innerText = `${horasUnaCifra}:${minutosUnaCifra}:${segundosUnaCifra}`;
   }
 
   function asignarCifra(unidadDeTiempo) {
-      return unidadDeTiempo < 10 ? '0' + unidadDeTiempo : unidadDeTiempo;
-      
- }
-    
+    return unidadDeTiempo < 10 ? "0" + unidadDeTiempo : unidadDeTiempo;
+  }
+  setInterval(actualizarCronometro, 1000);
+  return (
+    <div className="contenedor-principal">
+      <div className="counter-container" id='crono'>00:00:00</div>
+    </div>
+  );
+};
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,11 +3,10 @@ import React from "react";
 import { Counter } from "./counter";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-import marmol from "../../img/marmol.jpg";
 
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
 		<div className="body">
 			<Counter/>
